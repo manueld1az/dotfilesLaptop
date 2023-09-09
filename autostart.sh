a1cf54b3361a42b03ff4e62ea575a2eeea756ca8
manueld1az <diazmanuelj13@gmail.com>
2023-09-08 21:59:47 -0500

    feat: :sparkles: First commit of dotfiles for laptop's
    Configurate basic shortcuts, blue-d1az theme and sensors on the top toolbar with update checker, currently layout, thermal sensors, ram monitor, speed internet monitor, calendar and clock, sistray icons.

---
+++ autostart.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# systray battery icon
+cbatticon -u 30 &
+# systray volume
+volumeicon &
---
+++ settings/keys.py
@@ -0,0 +1,106 @@
+# Antonio Sarosi
+# https://youtube.com/c/antoniosarosi
+# https://github.com/antoniosarosi/dotfiles
+
+# Qtile keybindings
+
+from libqtile.config import Key
+from libqtile.command import lazy
+
+
+mod = "mod4"
+
+keys = [Key(key[0], key[1], *key[2:]) for key in [
+    # ------------ Window Configs ------------
+
+    # Switch between windows in current stack pane
+    ([mod], "j", lazy.layout.down()),
+    ([mod], "k", lazy.layout.up()),
+    ([mod], "h", lazy.layout.left()),
+    ([mod], "l", lazy.layout.right()),
+    ([mod], "space", lazy.layout.next()),
+    ([mod, "shift"], "space", lazy.layout.previous()),
+
+    # Change window sizes (MonadTall)
+    #([mod, "shift"], "l", lazy.layout.grow()),
+    #([mod, "shift"], "h", lazy.layout.shrink()),
+
+    # Toggle floating
+    ([mod, "shift"], "f", lazy.window.toggle_floating()),
+
+    # Move windows up or down in current stack
+    ([mod, "shift"], "j", lazy.layout.shuffle_down()),
+    ([mod, "shift"], "k", lazy.layout.shuffle_up()),
+    ([mod, "shift"], "h", lazy.layout.shuffle_left()),
+    ([mod, "shift"], "l", lazy.layout.shuffle_right()),
+
+    # Resizes windows and reset all sizes
+    ([mod, "control"], "h", lazy.layout.grow_left()),
+    ([mod, "control"], "l", lazy.layout.grow_right()),
+    ([mod, "control"], "j", lazy.layout.grow_down()),
+    ([mod, "control"], "k", lazy.layout.grow_up()),
+    ([mod], "n", lazy.layout.normalize()),
+
+    # Toggle between different layouts as defined below
+    ([mod], "Tab", lazy.next_layout()),
+    ([mod, "shift"], "Tab", lazy.prev_layout()),
+
+    # Kill window
+    ([mod], "w", lazy.window.kill()),
+
+    # Switch focus of monitors
+    ([mod], "period", lazy.next_screen()),
+    ([mod], "comma", lazy.prev_screen()),
+
+    # Restart Qtile
+    ([mod, "control"], "r", lazy.restart()),
+
+    ([mod, "control"], "q", lazy.shutdown()),
+    ([mod], "r", lazy.spawncmd()),
+
+    # --- Launch programs ---
+    ([mod], "c", lazy.spawn("code")),
+
+    # Menu
+    ([mod], "m", lazy.spawn("rofi -show drun")),
+
+    # Window Nav
+    ([mod, "shift"], "m", lazy.spawn("rofi -show")),
+
+    # Browser
+    #([mod], "b", lazy.spawn("firefox")),
+    ([mod], "b", lazy.spawn("brave")),
+
+    # File Explorer
+    #([mod], "e", lazy.spawn("thunar")),
+    ([mod], "e", lazy.spawn("pcmanfm")),
+
+    # Terminal
+    #([mod], "Return", lazy.spawn("kitty")),
+    ([mod], "Return", lazy.spawn("alacritty")),
+
+    # Redshift
+    #([mod], "r", lazy.spawn("redshift -O 2400")),
+    #([mod, "shift"], "r", lazy.spawn("redshift -x")),
+
+    # Screenshot
+    ([mod], "s", lazy.spawn("scrot")),
+    ([mod, "shift"], "s", lazy.spawn("scrot -s")),
+
+    # ------------ Hardware Configs ------------
+
+    # Volume
+    ([], "XF86AudioLowerVolume", lazy.spawn(
+        "pactl set-sink-volume @DEFAULT_SINK@ -5%"
+    )),
+    ([], "XF86AudioRaiseVolume", lazy.spawn(
+        "pactl set-sink-volume @DEFAULT_SINK@ +5%"
+    )),
+    ([], "XF86AudioMute", lazy.spawn(
+        "pactl set-sink-mute @DEFAULT_SINK@ toggle"
+    )),
+
+    # Brightness if you use a laptop
+    ([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl set +1%")),
+    ([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl set 1%-")),
+]]
